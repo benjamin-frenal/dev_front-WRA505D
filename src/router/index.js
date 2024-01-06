db39d915a1183a803db466b2936bdf5b87268f04
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2024-01-06 16:40:35 +0100

    Feature : Création des vues + liens Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Actors from '../views/Actors.vue'
+import Actor from '../views/Actor.vue'
+import Categories from '../views/Categories.vue'
+import Categorie from '../views/Categorie.vue'
+import Movies from '../views/Movies.vue'
+import Movie from '../views/Movie.vue'
+import LoginView from "../views/Login.vue";
+import AccountView from "../views/Account.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +15,70 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/account",
+      name: "account",
+      component: AccountView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/actors',
+      name: 'actors',
+      component: Actors,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/actor/:id',
+      name: 'actor',
+      component: Actor,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: Categories,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/categorie/:id',
+      name: 'categorie',
+      component: Categorie,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: Movies,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/movie/:id',
+      name: 'movie',
+      component: Movie,
+      meta: { requiresAuth: true },
+    },
   ]
 })
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const token = localStorage.token;
+    if (!token) {
+      next("/login");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
