85029ae427b6ec60f5b28c5b27c0e79e9b5d7079
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2024-01-24 08:37:32 +0100

    Feature : MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contour">
+    <div class="div" style="color: white">
+      <router-link :to="'/movie/' + movie.id" class="movie-bloc">
+        <img :src="movie.miniature" :alt="movie.title">
+      </router-link>
+    </div>
+    <div class="texte">
+      <p>{{ movie.title }}</p>
+      <p class="description">{{ movie.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps(['movie']);
+const movie = ref(props.movie);
+</script>
+
+<style scoped>
+
+</style>
