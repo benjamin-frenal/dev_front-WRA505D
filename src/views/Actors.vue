ed4dc4a48b9c4ecb3efef920dff5f4a7547ed6e7
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2024-02-15 12:01:06 +0100

    Feature > Ajout des acteurs

--- src/views/Actors.vue
+++ src/views/Actors.vue
@@ -1,8 +1,30 @@
 <template>
   <main>
     <div class="content--top">
-      <div class="flex">
+      <div class="flex sb">
         <h1>Acteurs</h1>
+        <button class="btn-add" @click="showModal = true">Ajouter un acteur <i class="fa-solid fa-plus"></i></button>
+      </div>
+    </div>
+    <div :class="[{ 'd-none Jmodal-modif': !showModal }, 'modal_modif']">
+      <div class="modal-content">
+        <span class="close" @click="showModal = false">&times;</span>
+        <h2>Ajouter un acteur</h2>
+        <form @submit.prevent="addActor">
+          <div class="form-group form-group-pen">
+            <label for="newActorFirstName">Prénom</label>
+            <input type="text" v-model="newActorFirstName" placeholder="Prénom" required>
+          </div>
+          <div class="form-group form-group-pen">
+            <label for="newActorLastName">Nom</label>
+            <input type="text" v-model="newActorLastName" placeholder="Nom" required>
+          </div>
+          <div class="form-group form-group-pen">
+            <label for="newActorImage">URL de l'image</label>
+            <input type="text" v-model="newActorImage" placeholder="URL de l'image">
+          </div>
+          <button class="btn" type="submit">Ajouter</button>
+        </form>
       </div>
     </div>
     <div class="list-acteurs">
@@ -24,6 +46,10 @@ import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
 let data = ref('')
+let showModal = ref(false)
+let newActorFirstName = ref('')
+let newActorLastName = ref('')
+let newActorImage = ref('')
 
 onMounted(async () => {
   try {
@@ -45,6 +71,36 @@ onMounted(async () => {
     console.error('Error fetching authors:', error)
   }
 })
+const addActor = async () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (!token){
+      this.$router.push('/');
+      return;
+    }
+
+    const response = await axios.post('https://127.0.0.1:8000/api/authors', {
+      firstName: newActorFirstName.value,
+      lastName: newActorLastName.value,
+      image: newActorImage.value
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      }
+    })
+
+    // Mettre à jour la liste des acteurs après l'ajout du nouvel acteur
+    data.value.push(response.data)
+    showModal.value = false // Fermer la modal après l'ajout réussi
+    // Effacer les champs de saisie
+    newActorFirstName.value = ''
+    newActorLastName.value = ''
+    newActorImage.value = ''
+  } catch (error) {
+    console.error('Error adding actor:', error)
+  }
+}
 </script>
 <style scoped>
 *{
